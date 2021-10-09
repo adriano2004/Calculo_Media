ccfc3d4bb152652db98633034bafee079184061b
Adriano dos Santos Lisboa <robertopereira23447@gmail.com>
2021-10-09 18:32:18 -0300

    Calculadora de Média

---
+++ CalculoMedia.py
@@ -0,0 +1,17 @@
+print("---------------------------------")
+print("|Calculo da Média Anual Escolar |")
+print("---------------------------------")
+
+aluno = str(input("Nome do aluno(a): "))
+nota1 = float(input("Nota do Primeiro Trimestre: "))
+nota2 = float(input("Nota do Primeiro Trimestre: "))
+nota3 = float(input("Nota do Primeiro Trimestre: "))
+
+media = (nota1 + nota2 + nota3)/3
+
+if media <= 7:
+    print("A média do aluno(a)", aluno, "é", media,)
+    print("O aluno(a)", aluno, "reprovou")
+else:
+    print("A média do aluno(a)", aluno, "é", media,)
+    print("O aluno", aluno, "passou!!")
