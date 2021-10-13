a5e3d4805cd0b2a2dcc1ab1311b01d0d494c7b42
Adriano dos Santos Lisboa <robertopereira23447@gmail.com>
2021-10-13 15:36:12 -0300

    Add files via upload

--- CalculoMedia.py
+++ CalculoMedia.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 print("---------------------------------")
 print("|Calculo da Média Anual Escolar |")
 print("---------------------------------")
@@ -7,11 +9,13 @@ nota1 = float(input("Nota do Primeiro Trimestre: "))
 nota2 = float(input("Nota do Primeiro Trimestre: "))
 nota3 = float(input("Nota do Primeiro Trimestre: "))
 
-media = (nota1 + nota2 + nota3)/3
+media = float((nota1 + nota2 + nota3)/3)
 
 if media <= 7:
     print("A média do aluno(a)", aluno, "é", media,)
     print("O aluno(a)", aluno, "reprovou")
 else:
-    print("A média do aluno(a)", aluno, "é", media,)
+    print("A média do aluno(a)", aluno, "é", media)
     print("O aluno", aluno, "passou!!")
+
+sleep(60)
